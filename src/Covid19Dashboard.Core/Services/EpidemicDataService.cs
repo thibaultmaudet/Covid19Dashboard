b8edba0240c08507f1a4a31f7a4350b3b7bbf2ee
Thibault MAUDET <41793338+thibaultmaudet@users.noreply.github.com>
2022-03-12 11:16:41 +0100

    Improving: Display vaccination data for the entire population.

--- src/Covid19Dashboard.Core/Services/EpidemicDataService.cs
+++ src/Covid19Dashboard.Core/Services/EpidemicDataService.cs
@@ -1,12 +1,12 @@
-﻿using Covid19Dashboard.Core.Models;
-using Covid19Dashboard.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
+
+using Covid19Dashboard.Core.Models;
+using Covid19Dashboard.Helpers;
 
 namespace Covid19Dashboard.Core.Services
 {
@@ -17,7 +17,7 @@ namespace Covid19Dashboard.Core.Services
             filePath = Path.Combine(filePath, Guid.NewGuid() + ".csv");
             
             // Download the summary of indicators for monitoring the COVID-19 epidemic csv file.
-            WebClient webClient = new WebClient();
+            WebClient webClient = new();
             if (area == Area.National)
                 webClient.DownloadFile("https://www.data.gouv.fr/fr/datasets/r/f335f9ea-86e3-4ffa-9684-93c009d5e617", filePath);
             else if (area == Area.Department)
@@ -31,12 +31,12 @@ namespace Covid19Dashboard.Core.Services
             filePath = Path.Combine(filePath, Guid.NewGuid() + ".csv");
 
             // Download the summary of indicators for monitoring the COVID-19 vaccination csv file.
-            WebClient webClient = new WebClient();
-            
+            WebClient webClient = new();
+
             if (area == Area.National)
-                webClient.DownloadFile("https://www.data.gouv.fr/fr/datasets/r/fa4ad329-14ec-4394-85a4-c5df33769dff", filePath);
+                webClient.DownloadFile("https://www.data.gouv.fr/fr/datasets/r/efe23314-67c4-45d3-89a2-3faef82fae90", filePath);
             else if (area == Area.Department)
-                webClient.DownloadFile("https://www.data.gouv.fr/fr/datasets/r/f77106ed-6e27-48cf-85b7-daad2b7fce1e", filePath);
+                webClient.DownloadFile("https://www.data.gouv.fr/fr/datasets/r/4f39ec91-80d7-4602-befb-4b522804c0af", filePath);
 
             return (await CsvHelper.ToObject<List<VaccinationIndicator>>(filePath, ';')).OrderByDescending(x => x.Date).ToList();
         }
