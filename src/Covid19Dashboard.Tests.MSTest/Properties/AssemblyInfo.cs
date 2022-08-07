cb32d4b0ceec0690df6f2e8e43161a7d05097a99
Thibault MAUDET <41793338+thibaultmaudet@users.noreply.github.com>
2022-08-07 19:03:32 +0200

    Adding: Test project.

---
+++ src/Covid19Dashboard.Tests.MSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Covid19Dashboard.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Covid19Dashboard.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform", "UAP")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ src/Covid19Dashboard.Tests.MSTest/Tests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Covid19Dashboard.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
